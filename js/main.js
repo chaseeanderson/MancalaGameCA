68073d10bbe7d99dccf50aef182601b70a4ff00f
Chase Anderson <anderson.e.chase@gmail.com>
2021-03-05 15:36:42 -0800

    Update to constants, state variables, cached elements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 /*----- constants -----*/
+const playerRef = {
+  '1': {
+    name: 'Player 1'
+  },
+  '-1': {
+    name: 'Player 2'
+  }
+};
+// TODO - get gem image
+// const gemsImg = 
+
+
 /*----- app's state (variables) -----*/
+let board, gems, turn, scores, winner; 
+
+
 /*----- cached element references -----*/
+let scoreEls = {
+  p1: document.querySelector('p1score'),
+  p2: document.querySelector('p2score')
+}
+
+let cellEls = [...document.querySelectorAll('main div')];
+console.log(cellEls)
+
+
 /*----- event listeners -----*/
+
+
 /*----- functions -----*/
