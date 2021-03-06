fcee1cef42edb816ec390f81c48f9a9f3b2fb6b2
Chase Anderson <anderson.e.chase@gmail.com>
2021-03-05 21:57:09 -0800

    remove css to re visualize data. add p1 click loop

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const playerRef = {
   '1': {
     name: 'Player 1'
-  },
+  },         
   '-1': {
     name: 'Player 2'
   }
@@ -17,25 +17,54 @@ let board, gems, turn, scores, winner;
 
 /*----- cached element references -----*/
 const scoreEls = {
-  p1: document.querySelector('p1score'),
-  p2: document.querySelector('p2score')
+  p1: document.getElementById('6'),
+  p2: document.getElementById('13')
 }
+
 const cellEls = [...document.querySelectorAll('main div')];
+
 const replayBtn = document.querySelector('button'); 
-const playableEls = [...document.querySelectorAll('#board div:nth-child(n+2):nth-child(-n+13)')]
+// const playableEls = [...document.querySelectorAll('#board div:nth-child(n+2):nth-child(-n+13)')]
 
 
 /*----- event listeners -----*/
-playableEls.forEach(el => el.addEventListener('click', handleSelect));
+// cellEls.forEach(el => el.addEventListener('click', player1Click));
+document.getElementById('board').addEventListener('click', player1Click);
 replayBtn.addEventListener('click', init);
 
 /*----- functions -----*/
 init()
 
+
+
 function handleSelect() {
 
-console.log('billygoat')
-  // render()
+
+  render()
+}
+
+
+
+function player1Click(e) {
+  const selection = cellEls.indexOf(e.target)
+  // Sets the amount of gems to be distributed
+  gems = board[selection];
+  board[selection] = 0;
+  // Distributes gems
+  while (gems > 0) {
+    for (let i = selection + 1; i < board.length; i++) {
+      gems--
+      board[i]++
+      if (board[i] === 13) break; // START HERE TODAY. LOOP ISNT STOPPING
+      render()
+    }
+  }
+  console.log(board)
+  
+  // const divId = document.getElementById(`${playableIdx}`)
+  // console.log(playableIdx)
+  // console.log(divId)
+  
 }
 
 function render () {
@@ -43,7 +72,11 @@ function render () {
 }
 
 function init () {
+board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0];
 
+gems = 0;
+turn = 1;
+winner = null;
 
   render()
 }
